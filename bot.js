49655474925bd352c57badc8340ac7c4d9313cf2
Eros <george.marr.cobc@gmail.com>
2018-12-26 23:29:50 +0000

    add in the command framework (if you can call it that)

--- bot.js
+++ bot.js
@@ -3,18 +3,30 @@ var config = require('./config.json');
 var unirest = require('unirest');
 var Discord = require('discord.js');
 const bot = new Discord.Client();
+const fs = require('fs');
 
 bot.login(config.discord_token);
+
+
+fs.readdir('./commands/', (err, files) => {
+    if(err)
+        console.log(err);
+    let jsFiles = files.filter(f => f.split('.').pop() === 'js');
+
+    if(jsFiles.length <= 0) {
+        console.log('Unable to load commands');
+        return;
+    }
+
+    jsFiles.forEach((f, i) => {
+        let props = require(`./commands/${f}`);
+        console.log(`Loaded ${f} command`);
+        bot.commands.set(props.help.name, props);
+    });
+});
 
 bot.on('ready', async => {
     console.log('\nInsultBot starting up...');
     bot.user.setActivity('&insult @name', {type: 'LISTENING'});
     console.log('\nBot has started!\n');
-
-   unirest.get("https://lakerolmaker-insult-generator-v1.p.rapidapi.com/?mode=random")
-        .header("Authorization", "FREE")
-        .header("X-RapidAPI-Key", config.rapid_api_token)
-        .end(function (result) {
-        console.log(result.raw_body)
-    });
 });
