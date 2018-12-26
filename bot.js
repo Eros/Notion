7341f6b14ce324123c110765b2ae07fb0aed3c25
Eros <george.marr.cobc@gmail.com>
2018-12-26 23:19:43 +0000

    finally it's working

--- bot.js
+++ bot.js
@@ -4,20 +4,17 @@ var unirest = require('unirest');
 var Discord = require('discord.js');
 const bot = new Discord.Client();
 
-bot.login(token);
+bot.login(config.discord_token);
 
 bot.on('ready', async => {
     console.log('\nInsultBot starting up...');
     bot.user.setActivity('&insult @name', {type: 'LISTENING'});
+    console.log('\nBot has started!\n');
 
-    genInsult();
+   unirest.get("https://lakerolmaker-insult-generator-v1.p.rapidapi.com/?mode=random")
+        .header("Authorization", "FREE")
+        .header("X-RapidAPI-Key", config.rapid_api_token)
+        .end(function (result) {
+        console.log(result.raw_body)
+    });
 });
-
-function getInsult(){
-    unirest.get("https://lakerolmaker-insult-generator-v1.p.rapidapi.com/?mode=random")
-.header("Authorization", "FREE")
-.header("X-RapidAPI-Key", "")
-.end(function (result) {
-  console.log(result.status, result.headers, result.body);
-});
-}
