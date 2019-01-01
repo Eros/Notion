9bd890e324836414bb5c224485417c74aedd2e8c
Eros <george.marr.cobc@gmail.com>
2019-01-01 13:27:56 +0000

    well something fucked up somewhere

--- commands/catto.js
+++ commands/catto.js
@@ -4,39 +4,40 @@ const r2 = require('r2');
 var config = require('../config.json');
 
 module.exports.run = (bot, message, args) => {
-
-    try {
-        var images = await loadImage(message.author.username);
-        var image = images[0];
-        message.channel.send({files:[image.url]});
-    } catch(error) {
-        console.log(error);
-    }
-
-    function loadImage(sub_id) {
-        var headers = { 'X-API-KEY': config.cat_api_key, }
-    
-        var query_params = {
-            'mime_types': 'jpg, png',
-            'size': 'med',
-            'sub_id': sub_id,
-            'limit': 1
-        }
-    
-        let queryString = querystring.stringify(query_params);
-    
-        try {
-            let _url = API_URL + `v1/images/search?${queryString}`
-            var response = await r2.get(_url, {headers}).json
-        } catch(e) {
-            console.log(e)
-        }
-        return response;
-    }
-
+    sendMessage(message);
 } 
 
 module.exports.help = {
     name: 'catto'
 }
 
+async function sendMessage(message) {
+    try {
+        var images = await this.loadImage(message.author.username);
+        var image = images[0];
+        message.channel.send({files:[image.url]});
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+async function loadImage(sub_id) {
+    var headers = { 'X-API-KEY': config.cat_api_key, }
+
+    var query_params = {
+        'mime_types': 'jpg, png',
+        'size': 'med',
+        'sub_id': sub_id,
+        'limit': 1
+    }
+
+    let queryString = querystring.stringify(query_params);
+
+    try {
+        let _url = API_URL + `v1/images/search?${queryString}`
+        var response = await r2.get(_url, {headers}).json
+    } catch(e) {
+        console.log(e)
+    }
+    return response;
+}
