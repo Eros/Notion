18e757ce392ed5779673e27362ff8c97ccd3e8a5
Eros <george.marr.cobc@gmail.com>
2019-01-01 13:52:35 +0000

    wow stack overflow was useful for once

--- commands/catto.js
+++ commands/catto.js
@@ -1,43 +1,18 @@
-const querystring = require('querystring');
-const API_URL = 'https://api.thecatapi.com/'
-const r2 = require('r2');
-var config = require('../config.json');
+const request = require('request');
 
 module.exports.run = (bot, message, args) => {
-    sendMessage(message);
+    request.get('http://thecatapi.com/api/images/get?format=src&type=png', {
+
+    }, function(error, response, body) {
+        if(!error && response.statusCode == 200) {
+            message.channel.send(response.request.uri.href);
+        } else {
+            console.log(error);
+        }
+    });
 } 
 
 module.exports.help = {
     name: 'catto'
 }
 
-async function sendMessage(message) {
-    try {
-        var images = await this.loadImage(message.author.username);
-        var image = images[0];
-        message.channel.send({files:[image.url]});
-    } catch(error) {
-        console.log(error);
-    }
-}
-
-async function loadImage(sub_id) {
-    var headers = { 'X-API-KEY': config.cat_api_key, }
-
-    var query_params = {
-        'mime_types': 'jpg, png',
-        'size': 'med',
-        'sub_id': sub_id,
-        'limit': 1
-    }
-
-    let queryString = querystring.stringify(query_params);
-
-    try {
-        let _url = API_URL + `v1/images/search?${queryString}`
-        var response = await r2.get(_url, {headers}).json
-    } catch(e) {
-        console.log(e)
-    }
-    return response;
-}
