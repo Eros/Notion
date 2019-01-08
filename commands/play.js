52741e8f04c33762d3f4b957410a805560980a29
Eros <george.marr.cobc@gmail.com>
2019-01-08 16:52:31 +0000

    would be nice if vsc would fucking save

--- commands/play.js
+++ commands/play.js
@@ -1,4 +1,5 @@
 const yt = require('ytdl-core');
+const config = require('../config.json')
 let queue = {}
 
 module.exports.run = (bot, message, args) => {
@@ -19,8 +20,45 @@ module.exports.run = (bot, message, args) => {
             return message.reply('Queue is empty!').then(() => {
                 queue[message.guild.id].playing = false;
                 message.member.voiceChannel.leave();
-            })
-    })
+            });
+        message.channel.send(`Now playing: ${song.title}`);
+        dispatcher = message.guild.voiceConnection.playStream(yt(song.url, {audioonly: true}), {passes: 1});
+        let collector = message.channel.createCollector(m => m);
+        collector.on('message', m => {
+            if(m.content.startsWith(config.perfix + 'pause')) {
+                message.channel.send('Paused current song').then(() => {
+                    dispatcher.pause();
+                });
+            } else if(message.content.startsWith(config.prefix + 'resume')) {
+                message.channel.send('Resuming current song').then(() => {
+                    dispatcher.resume();
+                });
+            } else if(message.content.startsWith(config.prefix + 'skip')) {
+                message.channel.send('Skipping current song').then(() => {
+                    dispatcher.end();
+                });
+            } 
+        });
+
+        dispatcher.on('end', () => {
+            collector.stop();
+            play(queue[message.guild.id].songs.shift());
+        })
+    });
+
+    if(message.content.startsWith(config.prefix + 'add')) {
+        let url = messsage.content.split(' ')[1];
+        if(url == '' || url === undefined)
+            return message.reply('Invalid URL!');
+        yt.getInfo(url, (err, info) => {
+            if(err)
+                return message.reply('Invalid YT link' + err);
+            if(!queue.hasOwnProperty(message.guild.id))
+                queueu[message.guild.id] = {}, queue[message.guild.id].playing = false, queuep[message.guild.id].songs = [];
+            queue[message.guild.id].songs.push({url: url, title: info.title, requester: message.author.username});
+            message.channel.send(`Added ${info.title} to the queue`);
+        });
+    }
 }
 
 module.exports.help = {
