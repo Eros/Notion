5ecca99ab3d7f3d267956fa40195b7fc4a63d8e8
George <george.marr.cobc@gmail.com>
2019-01-29 13:36:58 +0000

    going to run forever through the code instead

--- bot.js
+++ bot.js
@@ -1,12 +1,7 @@
-const config = require('./config.json');
+var config = require('./config.json');
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const fs = require('fs');
-const MongoClient = require('mongodb').MongoClient;
-const collection_name = 'Globally_Banned';
-const dbUtils = require('./mongoUtils');
-
-var db = undefined;
 
 bot.login(config.discord_token);
 
@@ -14,17 +9,17 @@ bot.commands = new Discord.Collection();
 
 fs.readdir('./commands/', (err, files) => {
     if(err)
-        console.log('[X] Error reading commands: \n' + err);
+        console.log('Error reading commands: \n' + err);
 
     let js = files.filter(f => f.split('.').pop() === 'js');
     if(js.length <= 0) {
-        console.log('[X] Unable to load commands');
+        console.log('Unable to load commands');
         return;
     }
 
     js.forEach((f, i) => {
         let props = require(`./commands/${f}`);
-        console.log(`[!] Loaded command: ${f}`);
+        console.log(`Loaded command: ${f}`);
         bot.commands.set(props.help.name, props);
     });
 });
@@ -46,39 +41,11 @@ bot.on('message', message => {
     let commandFile = bot.commands.get(cmd.slice(prefix.length));
 
     if(commandFile)
-        //if(chatPrecheck(message))
-        //    message.reply('You are globally banned, you cannot run this command!');
-        //else 
-         commandFile.run(bot, message, args);
+        commandFile.run(bot, message, args);
 });
 
 bot.on('ready', async => {
-    console.log('\n [+] InsultBot starting up...');
+    console.log('\nInsultBot starting up...');
     bot.user.setActivity('&insult @name', {type: 'PLAYING'});
-    console.log('\n [+] Bot has started!\n');
-
-    //this is just for debug and testing
-    findInDocument();
-
-    //mongo setup and handling
-    console.log('Setting up MongoDB...');
-    MongoClient.connect(config.mongo_url, function(err, client) {
-        if(err) {
-            throw err;
-            console.log('Could not connect to Mongo, error thrown!');
-        } else {
-            db = client.db('InsultBot');
-            console.log('Logged into MongoDB');
-        }
-    });
+    console.log('\nBot has started!\n');
 });
-
-bot.on('guildCreate', guild => {
-    console.info('Joined new server > ' + guild.name);
-    //some mongo stuff will go here eventually
-});
-
-function findInDocument() {
-    const cursor = db.collection(collection_name).find({test_string});
-    console.log(cursor);
-}
