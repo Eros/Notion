c9ca91a58cad55505f8ad4ac3fa85431566cf56e
George <george.marr.cobc@gmail.com>
2019-06-20 14:09:45 +0100

    add in the message if no one is tagged

--- commands/help.js
+++ commands/help.js
@@ -3,47 +3,16 @@ const Discord = require('discord.js');
 
 module.exports.run = (bot, message, args) => {
     if (message.content.startsWith(config.prefix)) {
-        //     message.channel.send({
-        //         embed: {
-        //             color: 8913151,
-        //             author: {
-        //                 name: bot.user.username,
-        //                 icon_url: bot.user.avatarURL
-        //             },
-        //             title: 'Help',
-        //             description: 'List of commands',
-        //             fields: [{
-        //                 name: '&insult @user',
-        //                 value: 'Generates an insult at the targetted user'
-        //             },
-        //             {
-        //                 name: '&help',
-        //                 value: 'Prints this message'
-        //             },
-        //             {
-        //                 name: '&sys',
-        //                 value: 'Prints stats on the bot and the server.'
-        //             },
-        //             {
-        //                 name: '&globalban',
-        //                 value: 'Globally bans a user. This is only for the bot developer to use'
-        //             }],
-        //             footer: {
-        //                 icon_url: bot.user.avatarURL,
-        //                 text: ''
-        //             }
-        //         }
-        //     });
-        // }
 
         const embed = new Discord.RichEmbed();
         embed.setTitle('InsultBot commands')
-            .setAuthor('Developed by https://twitter.com/RapidTheNerd')
+            .setAuthor('')
             .setColor(0x00AE86)
             .addField('» &help', 'Runs this command', true)
             .addField('» &insult @name', 'Generates an insult the target user', true)
             .addField('» &sys', 'Provides stats on the bot and AWS server')
-            .addField('» &cat', 'Just incase you piss someone off this will put a cat in chat');
+            .addField('» &cat', 'Just incase you piss someone off this will put a cat in chat')
+            .setFooter('Developed by https://twitter.com/RapidTheNerd');
         message.channel.send(embed);
     }
 }
--- commands/insult.js
+++ commands/insult.js
@@ -5,11 +5,14 @@ module.exports.run = (bot, message, args) => {
     if (message.content.startsWith(config.prefix)) {
         let member = message.mentions.members.first();
 
+        if (member == '' || member == null)
+            message.reply('You noob, you need to tag someone!');
+
         unirest.get("https://lakerolmaker-insult-generator-v1.p.rapidapi.com/?mode=random")
             .header("Authorization", "FREE")
             .header("X-RapidAPI-Key", config.rapid_api_token)
             .end(function (result) {
-                var insult = result.raw_body;
+                var insult = result.raw_body.toLowerCase();
                 message.channel.send(member + ' ' + insult);
             });
     }
