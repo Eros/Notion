56aa93d9a20a51c5d1e52d03e7956bd59ba13e69
George <george.marr.cobc@gmail.com>
2019-06-20 14:14:20 +0100

    move that check so that it doesn't run the insult anyway if someone isn't tagged

--- commands/insult.js
+++ commands/insult.js
@@ -5,15 +5,17 @@ module.exports.run = (bot, message, args) => {
     if (message.content.startsWith(config.prefix)) {
         let member = message.mentions.members.first();
 
-        if (member == '' || member == null)
-            message.reply('You noob, you need to tag someone!');
-
         unirest.get("https://lakerolmaker-insult-generator-v1.p.rapidapi.com/?mode=random")
             .header("Authorization", "FREE")
             .header("X-RapidAPI-Key", config.rapid_api_token)
             .end(function (result) {
                 var insult = result.raw_body.toLowerCase();
-                message.channel.send(member + ' ' + insult);
+
+                if (member == '' || member == null)
+                    message.reply('You noob, you need to tag someone!');
+                else {
+                    message.channel.send(member + ' ' + insult);
+                }
             });
     }
 }
