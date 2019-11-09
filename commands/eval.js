ea8311cc1db82c639c6b5fdc4fbcd30b173b6420
George <george.marr.cobc@gmail.com>
2019-11-09 15:12:13 +0000

    add in the eval command

---
+++ commands/eval.js
@@ -0,0 +1,28 @@
+const config = require('../config.json');
+
+module.exports.run = (bot, message, args) => {
+    if(message.content.startsWith(config.prefix)) {
+        if(message.author.id != '226423378817449985') {
+            message.reply('You do not have permission to run this command. Only bot developers do!');
+            return;
+        } else {
+            let script = args.join('');
+            message.reply(':alarm_clock: Running your script!').then((reply) => {
+                try {
+                    let result = eval(script);
+                    if(result) 
+                        reply.edit(':white_check_mark:')
+                    else
+                        reply.edit(':white_check_mark: Eval ran, but something went wrong with your script! [No data]');
+                } catch(e) {
+                    reply.edit(':x: could not evaluate your script!');
+                    console.error(e);
+                }
+            })
+        }
+    }
+}
+
+module.exports.help = {
+    name: 'eval'
+}
