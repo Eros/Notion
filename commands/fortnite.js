c1dc42cc0e8efae59d77c6377d3190db9d79e621
George <george.marr.cobc@gmail.com>
2019-11-17 18:12:41 +0000

    this is just gonna be a test for now

--- commands/fortnite.js
+++ commands/fortnite.js
@@ -2,11 +2,24 @@ const config = require('../config.json');
 const unirest = require('unirest');
 
 module.exports.run = (bot, message, args) => {
-    var name = "Ninja"; //for now this is just for testing, will be replaced with args
+    if(message.content.startsWith(config.prefix)) {
+        var name = "Ninja"; //for now this is just for testing, will be replaced with args
+        var req = unirest("GET", "https://fortnite-api.p.rapidapi.com/stats/" + name);
+        req.headers({
+            "x-rapidapi-host": "fortnite-api.p.rapidapi.com",
+            "x-rapidapi-key": config.rapid_api_token
+        });
 
+        req.end(function(res) {
+            if(res.error) {
+                console.log('[!] Error caught: ' + res.error);
+            }
+            console.log(res.body);
+        })
+    }
 };
 
 module.exports.help = {
     name: 'fortnite'
-}
+};
 
