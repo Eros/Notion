2401ddf74a03aab38385a2352b4add3b23769ee3
George <george.marr.cobc@gmail.com>
2019-11-17 18:10:07 +0000

    end me

--- commands/fortnite.js
+++ commands/fortnite.js
@@ -1,6 +1,8 @@
 const config = require('../config.json');
+const unirest = require('unirest');
 
 module.exports.run = (bot, message, args) => {
+    var name = "Ninja"; //for now this is just for testing, will be replaced with args
 
 };
 
