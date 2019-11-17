04505649b6fa2bfcc94b816297ab742a16a4d366
George <george.marr.cobc@gmail.com>
2019-11-17 21:11:02 +0000

    that should be fine i think

---
+++ commands/purge.js
@@ -0,0 +1,19 @@
+const config = require('../config.json');
+
+module.exports.run = (bot, message, args) => {
+    if(message.author.member.hasPermission('MANAGE_MESSAGES')) {
+        if(!args[0])
+            message.reply('You need to specify an amount! (100 max)');
+        if(args[0] > 100)
+            message.reply('You can only delete 100 messages at one time');
+        message.channel.bulkDelete(args[0]).then(() => {
+           message.channel.send(`Cleared ${args[0]} messages!`);
+        });
+    } else {
+        message.reply('You do not have permission to run this command!');
+    }
+};
+
+module.exports.help = {
+    name: 'purge'
+};
