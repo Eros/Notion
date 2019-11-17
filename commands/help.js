69b1b9003e2ab01576b4b04691c9e26acd2a3975
George <george.marr.cobc@gmail.com>
2019-11-17 17:00:10 +0000

    remake the help command, if i can make it pretty

--- commands/help.js
+++ commands/help.js
@@ -1,53 +1,19 @@
 const config = require('../config.json');
+const Discord = require('discord.js');
 
 module.exports.run = (bot, message, args) => {
     if (message.content.startsWith(config.prefix)) {
 
+        const embed = new Discord.RichEmbed();
+        embed.setTitle('List of commands');
+        embed.addField('** Fun commands **');
+        embed.addBlankField(true);
+        embed.addField('&insult @name     &boomer @name', 'Generate an insult at a user!     Tell someone they\'re a boomer');
 
-        message.channel.send({embed: {
-            color: 0x00AE86,
-            author: {
-                name: ' ',
-                icon_url: bot.user.avatarURL
-            },
-            title: 'Help',
-            description: 'Some information and commands list',
-            fields: [{
-                name: '» &help',
-                value: 'Runs this command'
-            },
-            {
-                name: '» &insult @name',
-                value: 'Generates an insult at the targetted user'
-            },
-            {
-                name: '» &boomer @name',
-                value: 'Let someone know they\'re a boomer!'
-            },
-            {
-                name: '» &sys',
-                value: 'Provides information on the bot and server it runs on'
-            },
-            {
-                name: '» &cat',
-                value: 'Provides a picture of a cat'
-            },
-            {
-                name: '» &dadjoke',
-                value: 'Provides a terrible dadjoke'
-            },
-            {
-                name: '» &announcements',
-                value: 'View updates/announcements on the bot from the bot developer.'
-            }],
-            footer: {
-                icon_url: bot.user.avatarURL,
-                text: 'Developed by https://twitter.com/RapidThenerd'
-            }
-        }});
+        message.channel.send(embed);
     }
-}
+};
 
 module.exports.help = {
     name: 'help'
-}
+};
