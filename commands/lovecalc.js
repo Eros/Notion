954a4655d2016eb3a74b6bf2dc0c2a60d7eb6fff
George <george.marr.cobc@gmail.com>
2019-11-17 18:35:32 +0000

    cool that works

--- commands/lovecalc.js
+++ commands/lovecalc.js
@@ -1,12 +1,11 @@
 const config = require('../config.json');
 
 module.exports.run = (bot, message, args) => {
-    if(message.contents.startWith(config.prefix)) {
+    if(message.content.startsWith(config.prefix)) {
         var first = message.mentions.members.first();
         var second = message.mentions.members.array()[1];
 
-        console.log(first);
-        console.log(second);
+
     }
 };
 
