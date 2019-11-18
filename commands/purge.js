6eab710890506c4c8faeba48e30b9e8727aabf02
George <george.marr.cobc@gmail.com>
2019-11-18 15:41:55 +0000

    hopefully this works

--- commands/purge.js
+++ commands/purge.js
@@ -5,13 +5,16 @@ module.exports.run = (bot, message, args) => {
         if (message.member.hasPermission('MANAGE_MESSAGES', false, true)) {
             if (!args[0])
                 message.reply('You need to specify an amount! (100 max)');
-            if (args[0] > 100)
+            else if (args[0] > 100)
                 message.reply('You can only delete 100 messages at one time');
-            message.channel.bulkDelete(args[0]).then(() => {
-                message.channel.send(`Cleared ${args[0]} messages!`);
-            });
-        } else {
-            message.reply('You do not have permission to run this command!');
+            else {
+                message.channel.bulkDelete(args[0]).then(() => {
+                    message.channel.send(`Cleared ${args[0]} messages!`);
+
+                });
+            } else {
+                message.reply('You do not have permission to run this command!');
+            }
         }
     }
 };
