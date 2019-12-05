8a51b690760b379ce7386af25a390199a62c9833
George <george.marr.cobc@gmail.com>
2019-12-05 18:51:47 +0000

    janky as fix for the definitions being too long

--- commands/urban.js
+++ commands/urban.js
@@ -9,11 +9,15 @@ module.exports.run = (bot, message, args) => {
             if(error)
                 console.log(error.message);
             else {
+                var def = entries[0].definition;
+                if(def.length > 1024) 
+                    def = entries[1].definition;
+                 
                const embed = new Discord.RichEmbed();
                 embed.setColor(5438002);
                 embed.setTitle('Urban dictionary');
                 embed.addField('Word', entries[0].word);
-                embed.addField('Definition', entries[0].definition);
+                embed.addField('Definition', def);
                 embed.addField('Example', entries[0].example);
                 embed.addField(':thumbsup:', entries[0].thumbs_up);
                 embed.addField(':thumbsdown:', entries[0].thumbs_down);
